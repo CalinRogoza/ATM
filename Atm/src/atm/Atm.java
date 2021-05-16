4f11b1f123e6d3432d7a9d61be17d15109fdccf3
Călin-Andrei Rogoza <calin.andrei7@yahoo.com>
2021-05-16 18:06:49 +0300

    Implemented methods for reading and verifying user's id and pin from config file, created new methods for checking the balance and for withdrawing money

--- Atm/src/atm/Atm.java
+++ Atm/src/atm/Atm.java
@@ -1,15 +1,23 @@
 package atm;
 
+import bank.BankAccount;
+
+import java.io.*;
 import java.util.InputMismatchException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Atm {
     private final Scanner scanner = new Scanner(System.in);
     private final String INVALID_OPTION = "Ati introdus o optiune inexistenta.";
     private final String ENTER_OPTION = "Introduceti optiunea dorita: ";
+    private double defaultRonAmount = 1000.0d;
+    private double defaultEuroAmount = 1000.0d;
+    private double defaultPoundAmount = 1000.0d;
+    private double defaultDollarAmount = 1000.0d;
 
     private void verifyOption(int pickedOption) {
-        if (pickedOption != 0 && pickedOption != 1 && pickedOption != 2 && pickedOption != 3) {
+        if (pickedOption < 0 || pickedOption > 3) {
             System.out.println("Ati introdus o optiune inexistenta. Tastati 0, 1, 2 sau 3.");
         }
     }
@@ -30,9 +38,8 @@ public class Atm {
         return answer == 1;
     }
 
-    private void performActionsForExistingClient() {
+    private int getActionsForExistingClient() {
         Menu menu = new Menu();
-
         int pickedOption = -1;
         do {
             System.out.println(ENTER_OPTION);
@@ -46,6 +53,7 @@ public class Atm {
             }
         }
         while (pickedOption < 0 || pickedOption > 3);
+        return pickedOption;
     }
 
     private void performActionsForIndirectClient() {
@@ -66,12 +74,101 @@ public class Atm {
         while (pickedOption < 0 || pickedOption > 2);
     }
 
+    private String[] getAccountDataById(String id, String pin) {
+        try (final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(new File(".").getCanonicalPath() + "/src/users.txt")));) {
+            String line = bufferedReader.readLine();
+            while (!line.equals("")) {
+                String[] tokens = line.split(",");
+                String currentId = tokens[0];
+                String currentPin = tokens[1];
+                if (currentId.equals(id) && currentPin.equals(pin)) {
+                    return tokens;
+                }
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException | NullPointerException e) {
+            System.err.println("Acest cont nu exista.");
+            System.err.flush();
+            return null;
+        }
+        return null;
+    }
+
+    private BankAccount getAccountWithCredentials() {
+        scanner.nextLine();
+        System.out.println("ID: ");
+        String searchedId = scanner.nextLine();
+        System.out.println("PIN: ");
+        String searchedPin = scanner.nextLine();
+        System.out.println(searchedId + " " + searchedPin);
+        String[] accountDataTokens = getAccountDataById(searchedId, searchedPin);
+        if (accountDataTokens != null) {
+            boolean blockedStatus = Boolean.parseBoolean(accountDataTokens[2]);
+            double ronCurrencyValue = Double.parseDouble(accountDataTokens[3]);
+            double euroCurrencyValue = Double.parseDouble(accountDataTokens[4]);
+            double poundCurrencyValue = Double.parseDouble(accountDataTokens[5]);
+            double dollarCurrencyValue = Double.parseDouble(accountDataTokens[6]);
+            return new BankAccount(accountDataTokens[0], accountDataTokens[1], blockedStatus, ronCurrencyValue, euroCurrencyValue, poundCurrencyValue, dollarCurrencyValue);
+        }
+        return null;
+    }
+
+    private boolean isSufficientAmount(int amount, String currency) {
+        switch (currency) {
+            case "ron":
+                if (this.defaultRonAmount < 10000 && amount > defaultRonAmount / 10) {
+                    System.out.println("Nu se poate extrage aceasta suma.");
+                    return false;
+                }
+                return true;
+            case "£":
+                if (this.defaultPoundAmount < 1000 && amount > defaultPoundAmount / 10) {
+                    System.out.println("Nu se poate extrage aceasta suma.");
+                    return false;
+                }
+                return true;
+            case "$":
+                if (this.defaultDollarAmount < 1000 && amount > defaultDollarAmount / 10) {
+                    System.out.println("Nu se poate extrage aceasta suma.");
+                    return false;
+                }
+                return true;
+            case "€":
+                if (this.defaultEuroAmount < 1000 && amount > defaultEuroAmount / 10) {
+                    System.out.println("Nu se poate extrage aceasta suma.");
+                    return false;
+                }
+                return true;
+        }
+        return false;
+    }
+
     public void run() {
         System.out.println("Sunteti client al bancii noastre? (0/1)");
         boolean answer = isClientOfTheBank();
 
         if (answer) {
-            performActionsForExistingClient();
+            //introducere id si pin
+            BankAccount bankAccount = getAccountWithCredentials();
+            System.out.println(bankAccount);
+            if (bankAccount != null) {
+                int chosenOption = getActionsForExistingClient();
+                switch (chosenOption) {
+                    case 0:
+                        System.out.println(bankAccount.checkBalance());
+                        break;
+                    case 1:
+                        scanner.nextLine();
+                        System.out.println("Introduceti suma si valuta: ");
+                        String amountToBeWithdrawn = scanner.nextLine().toLowerCase(); //100 $
+                        String[] currencyTokens = amountToBeWithdrawn.split(" ");
+                        int amount = Integer.parseInt(currencyTokens[0]);
+                        String currency = currencyTokens[1];
+                        if (isSufficientAmount(amount, currency)){
+                            bankAccount.withdrawAmount(amount,currency);
+                        }
+                }
+            }
         } else {
             performActionsForIndirectClient();
         }
--- Atm/src/bank/BankAccount.java
+++ Atm/src/bank/BankAccount.java
@@ -1,21 +1,105 @@
 package bank;
 
+import java.io.*;
+
 public class BankAccount {
-    private int id;
-    private int pin;
+    private String id;
+    private String pin;
     private boolean blocked;
     private double ron;
     private double euro;
-    private double lire;
-    private double dolari;
+    private double pounds;
+    private double dollar;
 
-    public BankAccount(int id, int pin,boolean blocked, double ron, double euro, double lire, double dolari) {
+    public BankAccount(String id, String pin, boolean blocked, double ron, double euro, double pounds, double dollar) {
         this.id = id;
         this.pin = pin;
         this.blocked = blocked;
         this.ron = ron;
         this.euro = euro;
-        this.lire = lire;
-        this.dolari = dolari;
+        this.pounds = pounds;
+        this.dollar = dollar;
+    }
+
+    public void updateUserInConfigFile(String id, String toBeReplaced) {
+        try (final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(new File(".").getCanonicalPath() + "/src/users.txt")))) {
+            StringBuffer inputBuffer = new StringBuffer();
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                String[] tokens = line.split(",");
+                String currentId = tokens[0];
+                if (currentId.equals(id)) {
+                    inputBuffer.append(toBeReplaced);
+                } else {
+                    inputBuffer.append(line);
+                }
+                inputBuffer.append('\n');
+                line = bufferedReader.readLine();
+            }
+            FileOutputStream fileOut = new FileOutputStream(new File(".").getCanonicalPath() + "/src/users.txt");
+            fileOut.write(inputBuffer.toString().getBytes());
+            fileOut.close();
+        } catch (Exception e) {
+            System.err.println("Nu s-a putut realiza actualizarea fisierului.");
+            System.err.flush();
+        }
+    }
+
+    public String checkBalance() {
+        return "Ron: " + ron + "\n" + "Euro: " + euro + "€\n" + "Lire: " + pounds + "£\n" + "Dolari: " + dollar + "$";
+    }
+
+    public boolean withdrawAmount(double quantity, String currency) {
+        if (quantity <= 0 || currency.equals("")) {
+            return false;
+        }
+        switch (currency) {
+            case "ron":
+                if (this.ron > quantity) {
+                    this.ron -= quantity;
+                    String toBeReplaced = id + "," + pin + "," + blocked + "," + ron + "," + euro + "," + pounds + "," + dollar;
+                    updateUserInConfigFile(this.id, toBeReplaced);
+                    return true;
+                }
+                break;
+            case "€":
+                if (this.euro > quantity) {
+                    this.euro -= quantity;
+                    String toBeReplaced = id + "," + pin + "," + blocked + "," + ron + "," + euro + "," + pounds + "," + dollar;
+                    updateUserInConfigFile(this.id, toBeReplaced);
+                    return true;
+                }
+                break;
+            case "£":
+                if (this.pounds > quantity) {
+                    this.pounds -= quantity;
+                    String toBeReplaced = id + "," + pin + "," + blocked + "," + ron + "," + euro + "," + pounds + "," + dollar;
+                    updateUserInConfigFile(this.id, toBeReplaced);
+                    return true;
+                }
+                break;
+            case "$":
+                if (this.dollar > quantity) {
+                    this.dollar -= quantity;
+                    String toBeReplaced = id + "," + pin + "," + blocked + "," + ron + "," + euro + "," + pounds + "," + dollar;
+                    updateUserInConfigFile(this.id, toBeReplaced);
+                    return true;
+                }
+                break;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "id='" + id + '\'' +
+                ", pin='" + pin + '\'' +
+                ", blocked=" + blocked +
+                ", ron=" + ron +
+                ", euro=" + euro +
+                ", lire=" + pounds +
+                ", dollar=" + dollar +
+                '}';
     }
 }
